488a82a1f7c3c685b5c7b747a034ff5d5fd3f177
Chiara <chiaraiors@gmail.com>
2022-01-26 14:28:35 +0100

    Add Admin and Guest layouts

---
+++ resources/views/guest/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="p-5 bg-light">
+    <div class="container">
+        <h1 class="display-3">
+            Il costruttore di siti più famoso al mondo.
+        </h1>
+        <p class="lead">
+            Il 43% dei siti web usa la tecnologia WordPress. Blogger, piccole attività e grandi aziende nella lista
+            Fortune 500 usano WordPress più di tutte le alternative messe insieme. Unisciti ai milioni di utenti che
+            hanno scelto WordPress.com.
+        </p>
+    </div>
+</div>
+
+@endsection
