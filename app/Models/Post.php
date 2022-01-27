36e6575727df2d5288a37b383aaaa0c69fd9a708
Chiara <chiaraiors@gmail.com>
2022-01-27 14:25:26 +0100

    Create relation one to many

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'image', 'author', 'content'];
+    protected $fillable = ['title', 'image', 'content'];
+
+    public function categoty(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
