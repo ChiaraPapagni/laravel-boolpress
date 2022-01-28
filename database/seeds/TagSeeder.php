dfc15d4f488f1276d4936fcc12e5d3f6da59d3b7
Chiara <chiaraiors@gmail.com>
2022-01-28 15:29:09 +0100

    Add migration and seeder for tags

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = [
+            'Software Engineer',
+            'Software Develope',
+            'Full Stack Devs',
+            'Front End Devs',
+            'Back End Devs',
+            'Web Developer',
+        ];
+
+        foreach ($tags as $tag) {
+            $_tag = new Tag();
+            $_tag->name = $tag;
+            $_tag->slug = Str::slug($_tag->name);
+            $_tag->save();
+        }
+    }
+}
