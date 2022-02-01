b0960b82e886e5d1cb27d428c361e648f4b68948
Chiara <chiaraiors@gmail.com>
2022-02-01 15:06:54 +0100

    Add function store in ContactController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -10,4 +10,23 @@ class ContactController extends Controller
     {
         return view('guest.contacts.form');
     }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|min:3|max:50',
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+
+        $contact = Contact::create($validated);
+
+        Mail::to('admin@boolpress.com')->send(
+            new MarkdownContactFormMail($contact)
+        );
+
+        return redirect()
+            ->back()
+            ->with('message', 'Thanks for your message!');
+    }
 }
